6f862d44c122cd686855616a6478fef20267e41c
abdulazizpro378 <abdujazizabdi045@gmail.com>
2023-08-24 06:25:27 +0500

    s

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,6 @@
 export const TOKEN = "PORTFOLIO_TOKEN";
 export const USER = "USER";
+export const REGISTER_TOKEN='REGISTER_TOKEN'
 
 export const API = "https://ap-portfolio-backend.up.railway.app/";
 export const ENDPOINT = API + "api/v1/";
