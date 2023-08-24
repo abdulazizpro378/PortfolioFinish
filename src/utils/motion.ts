d6bbc4c4ad341200af5dcbf5db523c1863ca9d67
abdulazizpro378 <abdujazizabdi045@gmail.com>
2023-08-24 05:50:39 +0500

    salom

--- src/utils/motion.ts
+++ src/utils/motion.ts
@@ -1,4 +1,206 @@
-export const headerVariants = {
+// export const headerVariants = {
+//   hidden: {
+//     opacity: 0,
+//     y: -50,
+//     transition: {
+//       type: "spring",
+//       stiffness: 300,
+//       damping: 140,
+//     },
+//   },
+//   show: {
+//     opacity: 1,
+//     y: 0,
+//     transition: {
+//       type: "spring",
+//       stiffness: 80,
+//       delay: 1,
+//     },
+//   },
+// };
+
+// export const slideIn = (direction, type, delay, duration) => ({
+//   hidden: {
+//     x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
+//     y: direction === "up" ? "100%" : direction === "down" ? "100%" : 0,
+//   },
+//   show: {
+//     x: 0,
+//     y: 0,
+//     transition: {
+//       type,
+//       delay,
+//       duration,
+//       ease: "easeOut",
+//     },
+//   },
+// });
+
+// export const staggerContainer = (staggerChildren, delayChildren) => ({
+//   hidden: {},
+//   show: {
+//     transition: {
+//       staggerChildren,
+//       delayChildren,
+//     },
+//   },
+// });
+
+// export const textVariant = (delay) => ({
+//   hidden: {
+//     y: 50,
+//     opacity: 0,
+//   },
+//   show: {
+//     y: 0,
+//     opacity: 1,
+//     transition: {
+//       type: "spring",
+//       duration: 1.25,
+//       delay,
+//     },
+//   },
+// });
+
+// export const textContainer = {
+//   hidden: {
+//     opacity: 0,
+//   },
+//   show: (i = 1) => ({
+//     opacity: 1,
+//     transition: { staggerChildren: 0.1, delayChildren: i * 0.1 },
+//   }),
+// };
+
+// export const textVariant2 = {
+//   hidden: {
+//     opacity: 0,
+//     y: 20,
+//   },
+//   show: {
+//     opacity: 1,
+//     y: 0,
+//     transition: {
+//       type: "tween",
+//       ease: "easeIn",
+//     },
+//   },
+// };
+
+// export const fadeIn = (direction, type, delay, duration) => ({
+//   hidden: {
+//     x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
+//     y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
+//     opacity: 0,
+//   },
+//   show: {
+//     x: 0,
+//     y: 0,
+//     opacity: 1,
+//     transition: {
+//       type,
+//       delay,
+//       duration,
+//       ease: "easeOut",
+//     },
+//   },
+// });
+
+// export const planetVariants = (direction) => ({
+//   hidden: {
+//     x: direction === "left" ? "-100%" : "100%",
+//     rotate: 120,
+//   },
+//   show: {
+//     x: 0,
+//     rotate: 0,
+//     transition: {
+//       type: "spring",
+//       duration: 1.8,
+//       delay: 0.5,
+//     },
+//   },
+// });
+
+// export const zoomIn = (delay, duration) => ({
+//   hidden: {
+//     scale: 0,
+//     opacity: 0,
+//   },
+//   show: {
+//     scale: 1,
+//     opacity: 1,
+//     transition: {
+//       type: "tween",
+//       delay,
+//       duration,
+//       ease: "easeOut",
+//     },
+//   },
+// });
+
+// export const footerVariants = {
+//   hidden: {
+//     opacity: 0,
+//     y: 50,
+//     transition: {
+//       type: "spring",
+//       stiffness: 300,
+//       damping: 140,
+//     },
+//   },
+//   show: {
+//     opacity: 1,
+//     y: 0,
+//     transition: {
+//       type: "spring",
+//       stiffness: 80,
+//       delay: 0.5,
+//     },
+//   },
+// };
+
+// export const draw = {
+//   hidden: { pathLength: 0, opacity: 0 },
+//   visible: (i) => {
+//     const delay = 1.5;
+//     return {
+//       pathLength: 1,
+//       opacity: 1,
+//       transition: {
+//         pathLength: { delay, type: "spring", duration: 5, bounce: 0 },
+//         opacity: { delay, duration: 0.01 },
+//       },
+//     };
+//   },
+// };
+
+// export const staggerChildren = {
+//   hidden: { opacity: 0 },
+//   show: {
+//     opacity: 1,
+//     transition: {
+//       staggerChildren: 0.5,
+//     },
+//   },
+// };
+
+// export const listItem = {
+//   hidden: { opacity: 0 },
+//   show: { opacity: 1 },
+// };
+
+// export const getMenuStyles = (menuOpened) => {
+//   if (document.documentElement.clientWidth <= 640) {
+//     console.log("outside of sidebar reached")
+//     return { right: !menuOpened && "-100%" };
+//   }
+// };
+
+
+import { Variants } from "framer-motion";
+
+export const headerVariants: Variants = {
   hidden: {
     opacity: 0,
     y: -50,
@@ -19,7 +221,7 @@ export const headerVariants = {
   },
 };
 
-export const slideIn = (direction, type, delay, duration) => ({
+export const slideIn = (direction: string, type: string, delay: number, duration: number): Variants => ({
   hidden: {
     x: direction === "left" ? "-100%" : direction === "right" ? "100%" : 0,
     y: direction === "up" ? "100%" : direction === "down" ? "100%" : 0,
@@ -36,7 +238,7 @@ export const slideIn = (direction, type, delay, duration) => ({
   },
 });
 
-export const staggerContainer = (staggerChildren, delayChildren) => ({
+export const staggerContainer = (staggerChildren: number, delayChildren: number): Variants => ({
   hidden: {},
   show: {
     transition: {
@@ -46,7 +248,7 @@ export const staggerContainer = (staggerChildren, delayChildren) => ({
   },
 });
 
-export const textVariant = (delay) => ({
+export const textVariant = (delay: number): Variants => ({
   hidden: {
     y: 50,
     opacity: 0,
@@ -62,7 +264,7 @@ export const textVariant = (delay) => ({
   },
 });
 
-export const textContainer = {
+export const textContainer: Variants = {
   hidden: {
     opacity: 0,
   },
@@ -72,7 +274,7 @@ export const textContainer = {
   }),
 };
 
-export const textVariant2 = {
+export const textVariant2: Variants = {
   hidden: {
     opacity: 0,
     y: 20,
@@ -87,7 +289,7 @@ export const textVariant2 = {
   },
 };
 
-export const fadeIn = (direction, type, delay, duration) => ({
+export const fadeIn = (direction: string, type: string, delay: number, duration: number): Variants => ({
   hidden: {
     x: direction === "left" ? 100 : direction === "right" ? -100 : 0,
     y: direction === "up" ? 100 : direction === "down" ? -100 : 0,
@@ -106,7 +308,7 @@ export const fadeIn = (direction, type, delay, duration) => ({
   },
 });
 
-export const planetVariants = (direction) => ({
+export const planetVariants = (direction: string): Variants => ({
   hidden: {
     x: direction === "left" ? "-100%" : "100%",
     rotate: 120,
@@ -122,7 +324,7 @@ export const planetVariants = (direction) => ({
   },
 });
 
-export const zoomIn = (delay, duration) => ({
+export const zoomIn = (delay: number, duration: number): Variants => ({
   hidden: {
     scale: 0,
     opacity: 0,
@@ -139,7 +341,7 @@ export const zoomIn = (delay, duration) => ({
   },
 });
 
-export const footerVariants = {
+export const footerVariants: Variants = {
   hidden: {
     opacity: 0,
     y: 50,
@@ -160,22 +362,22 @@ export const footerVariants = {
   },
 };
 
-export const draw = {
-  hidden: { pathLength: 0, opacity: 0 },
-  visible: (i) => {
-    const delay = 1.5;
-    return {
-      pathLength: 1,
-      opacity: 1,
-      transition: {
-        pathLength: { delay, type: "spring", duration: 5, bounce: 0 },
-        opacity: { delay, duration: 0.01 },
-      },
-    };
-  },
-};
+// export const draw: Variants = {
+//   hidden: { pathLength: 0, opacity: 0 },
+//   visible: (i: number) => {
+//     const delay = 1.5;
+//     return {
+//       pathLength: 1,
+//       opacity: 1,
+//       transition: {
+//         pathLength: { delay, type: "spring", duration: 5, bounce: 0 },
+//         opacity: { delay, duration: 0.01 },
+//       },
+//     };
+//   },
+// };
 
-export const staggerChildren = {
+export const staggerChildren: Variants = {
   hidden: { opacity: 0 },
   show: {
     opacity: 1,
@@ -185,16 +387,13 @@ export const staggerChildren = {
   },
 };
 
-export const listItem = {
+export const listItem: Variants = {
   hidden: { opacity: 0 },
   show: { opacity: 1 },
 };
 
-export const getMenuStyles = (menuOpened) => {
+export const getMenuStyles = (menuOpened: boolean) => {
   if (document.documentElement.clientWidth <= 640) {
-    console.log("outside of sidebar reached")
-    return { right: !menuOpened && "-100%" };
+    return { right: !menuOpened ? "-100%" : undefined };
   }
 };
-
-
